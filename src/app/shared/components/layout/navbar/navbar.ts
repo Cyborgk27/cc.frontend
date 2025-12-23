de2461185482ccf736656f51d3a34f49918d00fb
kcepeda <kcepeda@viamatica.com>
2025-12-22 23:36:10 -0500

    feat: se crear un navbar dinámico para que muestre el title de cada módulo

--- src/app/shared/components/layout/navbar/navbar.ts
+++ src/app/shared/components/layout/navbar/navbar.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { AuthState } from '../../../../core/services/auth-state';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +11,33 @@ import { Router } from '@angular/router';
   styleUrl: './navbar.css',
 })
 export class Navbar {
-  private authState = inject(AuthState);
+private authState = inject(AuthState);
   private router = inject(Router);
 
-  // Seleccionamos los datos reactivos del estado de autenticación
   public userFullName = this.authState.userFullName;
+  private menu = this.authState.navigation; // Acceso al signal de navegación
+
+  // 1. Detectamos la URL actual de forma reactiva
+  private currentUrl = toSignal(
+    this.router.events.pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+      map(e => e.urlAfterRedirects)
+    ),
+    { initialValue: this.router.url }
+  );
+
+  // 2. Calculamos el título dinámico
+  public moduleTitle = computed(() => {
+    const url = this.currentUrl();
+    
+    // Buscamos si la URL actual coincide con algún path de nuestro menú
+    const activeItem = this.menu().find((item: { path: string; }) => url.includes(item.path));
+    
+    if (url.includes('dashboard')) return 'Panel de Control';
+    
+    // Si lo encuentra, muestra el showName (ej: "Catálogos"), si no, un genérico
+    return activeItem ? activeItem.showName : 'Sistema';
+  });
   
   logout() {
     this.authState.logout();
